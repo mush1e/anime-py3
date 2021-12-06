e734ce3e52dfe1bf3f24b9d6226056b8a4558f75
Mustafa Siddiqui (mush1e) <mustafa.a.r.siddiqui@outlook.com>
2021-12-06 20:17:03 +0530

    basic structure --logging off for the day

---
+++ main.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+
+class Anime_Py ():
+    def __init__():
+        with open("index.html") as fp:
+            soup = BeautifulSoup(fp)
+        
+
+if __name__ == '__main__':
+    Anime_Py()
